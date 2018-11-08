1beeda29f388f429e25e38b67ee5575077e7594c
kayee <kayee.wong@noaa.gov>
2018-11-08 13:29:38 -0700

    1) Added Hydra option 2) Added Theia option 3) Removed tarball option and added local path and github option. Tested on Cheyenne.

--- compile.sh
+++ compile.sh
@@ -2,12 +2,10 @@
 ###############################################
 #
 # Oirg. Code: Ka Yee Wong (NOAA/GSD) Jun  2017
-# Edit. Code: Ka Yee Wong (NOAA/GSD) Jul  2017
-# Edit. Code: Ka Yee Wong (NOAA/GSD) Aug  2017
-# Edit. Code: Ka Yee Wong (NOAA/GSD) Aug  2018
-# Edit. Code: Ka Yee Wong (NOAA/GSD) Sep  2018
-# Tested on Theia:                   Jun  2017
-# Tested on cheyenne:                Sep  2018
+# Edit. Code: Ka Yee Wong (NOAA/GSD) Nov  2018
+# Tested on Theia:                   Nov  2018
+# Tested on cheyenne:                Nov  2018
+# Tested on Hydra by Tracy:          Nov  2018
 #
 ###############################################
 # Run at background:
@@ -21,14 +19,21 @@
 ###############################################
 #!!!!!!!!!!!!!!User Define Here!!!!!!!!!!!!!!!#
 ###############################################
-set TAR_FILE_NAME = "DTC_upp_v3.2"
-#set TAR_FILE_NAME = "prerelease-V3.2-betaB-20171113"
-#set TAR_FILE_NAME = "prerelease-V3.2-beatA-20171106"
-set TAR_FILE_EXT = ".tar"
-set CONFIG_OPTION = (7) #1)PGI(serial) 2)PGI(dmpar) 4)Intel(dmpar) 3)Intel(serial) 7)GNU(serial) 8)GNU(dmpar)
-#set CONFIG_OPTION = (4 3 8 7) #1)PGI(serial) 2)PGI(dmpar) 4)Intel(dmpar) 3)Intel(serial) 7)GNU(serial) 8)GNU(dmpar)
-#set CONFIG_OPTION = (4 3 7 8) #1)PGI(serial) 2)PGI(dmpar) 4)Intel(dmpar) 3)Intel(serial) 7)GNU(serial) 8)GNU(dmpar)
-#set CONFIG_OPTION = (1 2 4 3 7 8) #1)PGI(serial) 2)PGI(dmpar) 4)Intel(dmpar) 3)Intel(serial) 7)GNU(serial) 8)GNU(dmpar)
+set source = (3) # 1) source code from vlab 2) source code from local path 3) source code from github
+if ($source == 1)then
+set FILE_NAME = "DTC_UPP_vlab"  # Your preferred directory name
+set repository = "https://vlab.ncep.noaa.gov/redmine/projects/emc-post"
+else if ($source == 2)then
+set FILE_NAME = "DTC_UPP_local_path_test"  # Your preferred directory name
+set upppath = "/gpfs/fs1/work/kayee/UPP/UFFDA/UPPV3.2"  # Local path that you want to copy from (no tar file)
+else if ($source == 3)then
+set FILE_NAME = "DTC_UPP_github_crtm"  # Your preferred directory name
+#set FILE_NAME = "DTC_UPP_github_v6f54859"  # Your preferred directory name
+set repository = "https://github.com/NCAR/UPP"
+endif
+set COMPUTER_OPTION = "CHEYENNE" # theia/cheyenne/hydra for now 
+set CONFIG_OPTION = (4 3 7 8) #1)PGI(serial) 2)PGI(dmpar) 3)Intel(serial) 4)Intel(dmpar) 7)GNU(serial) 8)GNU(dmpar) 11)GNU(serial) on Hydra 12)GNU(dmpar) on Hydra
+#set CONFIG_OPTION = (1 2 4 3 7 8) #1)PGI(serial) 2)PGI(dmpar) 3)Intel(serial) 4)Intel(dmpar) 7)GNU(serial) 8)GNU(dmpar) 11)GNU(serial) on Hydra 12)GNU(dmpar) on Hydra
 ###############################################
 ###############################################
 #!!!!!!!!!STOP User Define Here!!!!!!!!!!!!!!!#
@@ -36,72 +41,130 @@ set CONFIG_OPTION = (7) #1)PGI(serial) 2)PGI(dmpar) 4)Intel(dmpar) 3)Intel(seria
 ###############################################
 set NUM_CONFIG = $#CONFIG_OPTION  # # of configurations
 echo 'NUM_CONFIG = ' $NUM_CONFIG
+echo 'CONFIG_OPTION = ' $CONFIG_OPTION
 set UPP_CONFIGURE_COMMAND="./configure"
 
-set tarname = `tar --exclude="*/*" -tf $TAR_FILE_NAME$TAR_FILE_EXT`
-#set tarname = `tar --exclude="*/*" -tf ${TAR_FILE_NAME}${TAR_FILE_EXT}`
-set dirname = `echo $tarname | rev | cut -c 2- | rev`
+echo 'You are compiling the UPP code on' $COMPUTER_OPTION'.'
+if ($source == 1)then
+rm -rf $FILE_NAME
+git clone $repository $FILE_NAME
+else if ($source == 2)then
+rm -rf $FILE_NAME
+cp -ra $upppath $FILE_NAME
+cd $FILE_NAME
+./clean -a
+cd ../
+else if ($source == 3)then
+git clone --recurse-submodules $repository $FILE_NAME
+endif
 
 set i = 1
 while ($i <= $NUM_CONFIG)
 if($CONFIG_OPTION[$i] == 4)then
-rm -rf ${TAR_FILE_NAME}_Intel_dmpar
-tar -xvf $TAR_FILE_NAME$TAR_FILE_EXT
-mv $dirname ${TAR_FILE_NAME}_Intel_dmpar
-cd ${TAR_FILE_NAME}_Intel_dmpar
-module load intel
-#cd UPPV3.1_config$CONFIG_OPTION[$i]
-set targetDir='/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_dmpar_large-file'
+rm -rf ${FILE_NAME}_Intel_dmpar
+cp -ra ${FILE_NAME} ${FILE_NAME}_Intel_dmpar
+cd ${FILE_NAME}_Intel_dmpar
+if ($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+module purge
+module load intel/16.1.150
+module load impi
+module load netcdf/3.6.3
+set targetDir='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_Intel_dmpar_large-file'
+else if ($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+module purge
+module load intel mpt netcdf ncarcompilers
+set targetDir='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_dmpar_large-file'
+else if ($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+#source /home/hertneky/wheezy-intel.csh
+echo 'Note: No WRF bulit on hydra with Intel!!'
+endif
 else if($CONFIG_OPTION[$i] == 3)then
-rm -rf ${TAR_FILE_NAME}_Intel_serial
-tar -xvf $TAR_FILE_NAME$TAR_FILE_EXT
-mv $dirname ${TAR_FILE_NAME}_Intel_serial
-cd ${TAR_FILE_NAME}_Intel_serial
-module load intel
-set targetDir='/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_serial_large-file'
-else if($CONFIG_OPTION[$i] == 7)then
-rm -rf ${TAR_FILE_NAME}_GNU_serial
-tar -xvf $TAR_FILE_NAME$TAR_FILE_EXT
-mv $dirname ${TAR_FILE_NAME}_GNU_serial
-cd ${TAR_FILE_NAME}_GNU_serial
-module load gnu
-set targetDir='/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_serial_large-file'
-else if($CONFIG_OPTION[$i] == 8)then
-rm -rf ${TAR_FILE_NAME}_GNU_dmpar
-tar -xvf $TAR_FILE_NAME$TAR_FILE_EXT
-mv $dirname ${TAR_FILE_NAME}_GNU_dmpar
-cd ${TAR_FILE_NAME}_GNU_dmpar
-module load gnu
-set targetDir='/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_dmpar_large-file'
+rm -rf ${FILE_NAME}_Intel_serial
+cp -ra ${FILE_NAME} ${FILE_NAME}_Intel_serial
+cd ${FILE_NAME}_Intel_serial
+if ($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+module purge
+module load intel/16.1.150
+module load netcdf/3.6.3
+set targetDir='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_Intel_serial_large-file/'
+else if ($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+module purge
+module load intel netcdf ncarcompilers
+set targetDir='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_serial_large-file'
+else if ($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+#source /home/hertneky/wheezy-intel.csh
+echo 'Note: No WRF bulit on hydra with Intel!!'
+endif
+else if($CONFIG_OPTION[$i] == 7 || $CONFIG_OPTION[$i] == 11)then
+rm -rf ${FILE_NAME}_GNU_serial
+cp -ra ${FILE_NAME} ${FILE_NAME}_GNU_serial
+cd ${FILE_NAME}_GNU_serial
+pwd
+if ($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+echo 'Note: No WRF serial GNU bulit on Theia!!'
+else if ($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+module purge
+module load gnu netcdf ncarcompilers
+set targetDir='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_serial_large-file'
+echo $targetDir
+else if ($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+source /home/hertneky/wheezy-gf.csh
+set targetDir='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.32/em_real/WRFV3'
+endif
+else if($CONFIG_OPTION[$i] == 8 || $CONFIG_OPTION[$i] == 12)then
+rm -rf ${FILE_NAME}_GNU_dmpar
+cp -ra ${FILE_NAME} ${FILE_NAME}_GNU_dmpar
+cd ${FILE_NAME}_GNU_dmpar
+if ($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+echo 'Note: No WRF parallel GNU bulit on Theia!!'
+else if ($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+module purge
+module load gnu mpt netcdf ncarcompilers
+set targetDir='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_dmpar_large-file'
+else if ($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+source /home/hertneky/wheezy-gf.csh
+set targetDir='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.34/em_real/WRFV3'
+endif
 else if($CONFIG_OPTION[$i] == 1)then
-rm -rf ${TAR_FILE_NAME}_PGI_serial
-tar -xvf $TAR_FILE_NAME$TAR_FILE_EXT
-mv $dirname ${TAR_FILE_NAME}_PGI_serial
-cd ${TAR_FILE_NAME}_PGI_serial
-module load pgi
-set targetDir='/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_serial_large-file'
+rm -rf ${FILE_NAME}_PGI_serial
+cp -ra ${FILE_NAME} ${FILE_NAME}_PGI_serial
+cd ${FILE_NAME}_PGI_serial
+if ($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+module purge
+module load pgi/17.1
+module load netcdf/3.6.3
+set targetDir='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_PGI_serial_large-file/'
+else if ($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+module purge
+module load pgi netcdf ncarcompilers
+set targetDir='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_serial_large-file'
+else if ($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+source /home/hertneky/wheezy-pgi.csh
+set targetDir='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.1/em_real/WRFV3'
+endif
 else if($CONFIG_OPTION[$i] == 2)then
-rm -rf ${TAR_FILE_NAME}_PGI_dmpar
-tar -xvf $TAR_FILE_NAME$TAR_FILE_EXT
-mv $dirname ${TAR_FILE_NAME}_PGI_dmpar
-cd ${TAR_FILE_NAME}_PGI_dmpar
-module load pgi
-set targetDir='/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_dmpar_large-file'
+rm -rf ${FILE_NAME}_PGI_dmpar
+cp -ra ${FILE_NAME} ${FILE_NAME}_PGI_dmpar
+cd ${FILE_NAME}_PGI_dmpar
+if ($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+module purge
+module load pgi/17.1
+module load mvapich2/2.1rc1
+module load netcdf/3.6.3
+set targetDir='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_PGI_dmpar_large-file/'
+else if ($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+module purge
+module load pgi mpt netcdf ncarcompilers
+set targetDir='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_dmpar_large-file'
+else if ($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+source /home/hertneky/wheezy-pgi.csh
+set targetDir='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.3/em_real/WRFV3'
+endif
 endif
 setenv WRF_DIR $targetDir
 #pwd
 
 #./clean
-
-
-#./configure
-## THEIA #
-##set newFC = 'mpiifort'
-##sed -i -e "/DM_FC  =/ s|= .*|= $newFC|" configure.upp
-##set newF90 = 'mpiifort -free'
-#sed -i -e "/DM_F90 =/ s|= .*|= $newF90|" configure.upp
-##set newCC = 'mpiicc'
-##sed -i -e "/DM_CC  =/ s|= .*|= $newCC|" configure.upp
 
 # Put interactive "configure" options in a file, then pass to configure.
 cat > ./CONFIG_OPTIONS << EOF
@@ -111,6 +174,17 @@ EOF
 #EOF
 
 $UPP_CONFIGURE_COMMAND < ./CONFIG_OPTIONS
+
+if ($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+if($CONFIG_OPTION[$i] == 4)then
+set newFC = 'mpiifort'
+sed -i -e "/DM_FC  =/ s|= .*|= $newFC|" configure.upp
+set newF90 = 'mpiifort -free'
+sed -i -e "/DM_F90 =/ s|= .*|= $newF90|" configure.upp
+set newCC = 'mpiicc'
+sed -i -e "/DM_CC  =/ s|= .*|= $newCC|" configure.upp
+endif
+endif
 
 date > StartTime.txt
 ./compile > compile.log 
@@ -127,6 +201,8 @@ endif
 cd ../
 @ i++
 end
+
+rm -rf ${FILE_NAME} 
 
 echo 'Exit'
 exit
--- run.sh
+++ run.sh
@@ -2,12 +2,10 @@
 ###############################################
 #
 # Oirg. Code: Ka Yee Wong  (NOAA/GSD) Jun  2017
-# Edit. Code: Ka Yee Wong  (NOAA/GSD) Jul  2017
-# Edit. Code: Ka Yee Wong  (NOAA/GSD) Aug  2017
-# Edit. Code: Ka Yee Wong (NOAA/GSD) Aug  2018
-# Edit. Code: Ka Yee Wong (NOAA/GSD) Sep  2018
+# Edit. Code: Ka Yee Wong  (NOAA/GSD) Nov  2018
 # Tested on Theia:                    Jun  2017
-# Tested on cheyenne:                 Sep  2018
+# Tested on cheyenne:                 Nov  2018
+# Tested on Hydra by Tracy:           Nov  2018
 #
 ###############################################
 # Run at background:
@@ -22,15 +20,15 @@
 ###############################################
 #!!!!!!!!!!!!!!User Define Here!!!!!!!!!!!!!!!#
 ###############################################
- set TAR_FILE_NAME = "DTC_upp_v3.2"
- set outFormat = 'grib'
- set RunPath = 'your_path'
- set numR  = 10  # # of test cases
- #set CONFIG_OPTION = (4) # 4)Intel(dmpar) 3)Intel(serial) 7)GNU(serial) 8)GNU(dmpar) 1)PGI(serial) 2) PGI(dmpar)
- set CONFIG_OPTION = (3 4 7 8) # 4)Intel(dmpar) 3)Intel(serial) 7)GNU(serial) 8)GNU(dmpar) 1)PGI(serial) 2) PGI(dmpar)
- #set CONFIG_OPTION = (4 3 7 8 1 2) # 4)Intel(dmpar) 3)Intel(serial) 7)GNU(serial) 8)GNU(dmpar) 1)PGI(serial) 2) PGI(dmpar)
- set DataPath = '/gpfs/p/ral/jnt/UPP/data/wrf2008/'
- set ExtraPath = 'your_path'
+ set FILE_NAME = "DTC_UPP_github_crtm"   # should match the FILE_NAME from compile.sh
+ #set FILE_NAME = "DTC_UPP_local_path_test"
+ set outFormat = 'grib2' # 'grib' or 'grib2'
+ set RunPath = '/gpfs/fs1/work/kayee/tmp/' #Set your path for the test case results to be located
+ set numR  = 2  # # of test cases
+ set COMPUTER_OPTION = "cheyenne" # theia/cheyenne/hydra for now
+ set CONFIG_OPTION = (3 4 7 8) #1)PGI(serial) 2)PGI(dmpar) 3)Intel(serial) 4)Intel(dmpar) 7)GNU(serial) 8)GNU(dmpar) 11)GNU(serial) on Hydra 12)GNU(dmpar) on Hydra
+ set DataPath = '/gpfs/fs1/p/ral/jntp/UPP/data/wrf2008/'
+ set ExtraPath = '/gpfs/fs1/p/ral/jntp/UPP/public/'
  set lastfhr  = 18
  #set lastfhr  = 24
  set sleepT = 5
@@ -44,52 +42,125 @@
  set ii = 1
  while ($ii <= $NUM_CONFIG)
  if($CONFIG_OPTION[$ii] == 4)then
- rm -rf ${TAR_FILE_NAME}_test_cases_Intel_dmpar_${outFormat}
- mkdir ${TAR_FILE_NAME}_test_cases_Intel_dmpar_${outFormat}
- cd ${TAR_FILE_NAME}_test_cases_Intel_dmpar_${outFormat}
- eval set UPPPath = ${RunPath}\'${TAR_FILE_NAME}_Intel_dmpar/\'
- eval set CasesDir = ${RunPath}\'${TAR_FILE_NAME}_test_cases_Intel_dmpar_${outFormat}/\'
- set WRF_DIR = '/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_dmpar_large-file'
+ rm -rf ${FILE_NAME}_test_cases_Intel_dmpar_${outFormat}
+ mkdir ${FILE_NAME}_test_cases_Intel_dmpar_${outFormat}
+ cd ${FILE_NAME}_test_cases_Intel_dmpar_${outFormat}
+ eval set UPPPath = ${RunPath}\'${FILE_NAME}_Intel_dmpar/\'
+ eval set CasesDir = ${RunPath}\'${FILE_NAME}_test_cases_Intel_dmpar_${outFormat}/\'
  echo $CasesDir
- else if($CONFIG_OPTION[$ii] == 3)then
- rm -rf ${TAR_FILE_NAME}_test_cases_Intel_serial_${outFormat}
- mkdir ${TAR_FILE_NAME}_test_cases_Intel_serial_${outFormat}
- cd ${TAR_FILE_NAME}_test_cases_Intel_serial_${outFormat}
- eval set UPPPath = ${RunPath}\'${TAR_FILE_NAME}_Intel_serial/\'
- eval set CasesDir = ${RunPath}\'${TAR_FILE_NAME}_test_cases_Intel_serial_${outFormat}/\'
- set WRF_DIR = '/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_serial_large-file'
- else if($CONFIG_OPTION[$ii] == 7)then
- rm -rf ${TAR_FILE_NAME}_test_cases_GNU_serial_${outFormat}
- mkdir ${TAR_FILE_NAME}_test_cases_GNU_serial_${outFormat}
- cd ${TAR_FILE_NAME}_test_cases_GNU_serial_${outFormat}
- eval set UPPPath = ${RunPath}\'${TAR_FILE_NAME}_GNU_serial/\'
- eval set CasesDir = ${RunPath}\'${TAR_FILE_NAME}_test_cases_GNU_serial_${outFormat}/\'
- set WRF_DIR = '/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_serial_large-file'
- else if($CONFIG_OPTION[$ii] == 8)then
- rm -rf ${TAR_FILE_NAME}_test_cases_GNU_dmpar_${outFormat}
- mkdir ${TAR_FILE_NAME}_test_cases_GNU_dmpar_${outFormat}
- cd ${TAR_FILE_NAME}_test_cases_GNU_dmpar_${outFormat}
- eval set UPPPath = ${RunPath}\'${TAR_FILE_NAME}_GNU_dmpar/\'
- eval set CasesDir = ${RunPath}\'${TAR_FILE_NAME}_test_cases_GNU_dmpar_${outFormat}/\'
- set WRF_DIR = '/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_dmpar_large-file'
- else if($CONFIG_OPTION[$ii] == 2)then
- rm -rf ${TAR_FILE_NAME}_test_cases_PGI_dmpar_${outFormat}
- mkdir ${TAR_FILE_NAME}_test_cases_PGI_dmpar_${outFormat}
- cd ${TAR_FILE_NAME}_test_cases_PGI_dmpar_${outFormat}
- eval set UPPPath = ${RunPath}\'${TAR_FILE_NAME}_PGI_dmpar/\'
- eval set CasesDir = ${RunPath}\'${TAR_FILE_NAME}_test_cases_PGI_dmpar_${outFormat}/\'
- set WRF_DIR = '/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_dmpar_large-file'
- else if($CONFIG_OPTION[$ii] == 1)then
- rm -rf ${TAR_FILE_NAME}_test_cases_PGI_serial_${outFormat}
- mkdir ${TAR_FILE_NAME}_test_cases_PGI_serial_${outFormat}
- cd ${TAR_FILE_NAME}_test_cases_PGI_serial_${outFormat}
- eval set UPPPath = ${RunPath}\'${TAR_FILE_NAME}_PGI_serial/\'
- eval set CasesDir = ${RunPath}\'${TAR_FILE_NAME}_test_cases_PGI_serial_${outFormat}/\'
- set WRF_DIR = '/gpfs/p/ral/jnt/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_serial_large-file'
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+ module purge
+ module load intel/16.1.150
+ module load impi
+ module load netcdf/3.6.3
+ set WRF_DIR='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_Intel_dmpar_large-file'
+ else if($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+ module purge
+ module load intel mpt netcdf ncarcompilers
+ #setenv LD_LIBRARY_PATH "${LD_LIBRARY_PATH}:/glade/u/apps/ch/opt/netcdf/4.6.1/intel/17.0.1/lib"
+ set WRF_DIR='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_dmpar_large-file'
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+#source /home/hertneky/wheezy-intel.csh
+ echo 'Note: No WRF bulit on hydra with Intel!!'
  endif
- rm -rf case*
+ else if($CONFIG_OPTION[$ii] == 3)then
+ rm -rf ${FILE_NAME}_test_cases_Intel_serial_${outFormat}
+ mkdir ${FILE_NAME}_test_cases_Intel_serial_${outFormat}
+ cd ${FILE_NAME}_test_cases_Intel_serial_${outFormat}
+ eval set UPPPath = ${RunPath}\'${FILE_NAME}_Intel_serial/\'
+ eval set CasesDir = ${RunPath}\'${FILE_NAME}_test_cases_Intel_serial_${outFormat}/\'
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+ module purge
+ module load intel/16.1.150
+ module load netcdf/3.6.3
+ set WRF_DIR='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_Intel_serial_large-file/'
+ else if($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+ module purge
+ module load intel netcdf ncarcompilers
+ set WRF_DIR='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_Intel_serial_large-file'
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+ #source /home/hertneky/wheezy-intel.csh
+ echo 'Note: No WRF bulit on hydra with Intel!!'
+ endif
+ else if($CONFIG_OPTION[$ii] == 7 || $CONFIG_OPTION[$ii] == 11)then
+ rm -rf ${FILE_NAME}_test_cases_GNU_serial_${outFormat}
+ mkdir ${FILE_NAME}_test_cases_GNU_serial_${outFormat}
+ cd ${FILE_NAME}_test_cases_GNU_serial_${outFormat}
+ eval set UPPPath = ${RunPath}\'${FILE_NAME}_GNU_serial/\'
+ eval set CasesDir = ${RunPath}\'${FILE_NAME}_test_cases_GNU_serial_${outFormat}/\'
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+ echo 'Note: No WRF serial GNU bulit on Theia!!'
+ else if($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+ module purge
+ module load gnu netcdf ncarcompilers
+ set WRF_DIR='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_serial_large-file'
+ module load grads
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+ source /home/hertneky/wheezy-gf.csh
+ set WRF_DIR='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.32/em_real/WRFV3'
+ endif
+ else if($CONFIG_OPTION[$ii] == 8 || $CONFIG_OPTION[$ii] == 12)then
+ rm -rf ${FILE_NAME}_test_cases_GNU_dmpar_${outFormat}
+ mkdir ${FILE_NAME}_test_cases_GNU_dmpar_${outFormat}
+ cd ${FILE_NAME}_test_cases_GNU_dmpar_${outFormat}
+ eval set UPPPath = ${RunPath}\'${FILE_NAME}_GNU_dmpar/\'
+ eval set CasesDir = ${RunPath}\'${FILE_NAME}_test_cases_GNU_dmpar_${outFormat}/\'
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+ echo 'Note: No WRF parallel GNU bulit on Theia!!'
+ else if($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+ module purge
+ module load gnu mpt netcdf ncarcompilers
+ set WRF_DIR='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_GNU_dmpar_large-file'
  module load grads
  module load mpt
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+ source /home/hertneky/wheezy-gf.csh
+ set WRF_DIR='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.34/em_real/WRFV3'
+ endif
+ else if($CONFIG_OPTION[$ii] == 2)then
+ rm -rf ${FILE_NAME}_test_cases_PGI_dmpar_${outFormat}
+ mkdir ${FILE_NAME}_test_cases_PGI_dmpar_${outFormat}
+ cd ${FILE_NAME}_test_cases_PGI_dmpar_${outFormat}
+ eval set UPPPath = ${RunPath}\'${FILE_NAME}_PGI_dmpar/\'
+ eval set CasesDir = ${RunPath}\'${FILE_NAME}_test_cases_PGI_dmpar_${outFormat}/\'
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+ module purge
+ module load pgi/17.1
+ module load mvapich2/2.1rc1
+ module load netcdf/3.6.3
+ set WRF_DIR='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_PGI_dmpar_large-file/'
+ else if($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+ module purge
+ module load pgi mpt netcdf ncarcompilers
+ set WRF_DIR='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_dmpar_large-file'
+ module load grads
+ module load mpt
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+ source /home/hertneky/wheezy-pgi.csh
+ set WRF_DIR='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.3/em_real/WRFV3'
+ endif
+ else if($CONFIG_OPTION[$ii] == 1)then
+ rm -rf ${FILE_NAME}_test_cases_PGI_serial_${outFormat}
+ mkdir ${FILE_NAME}_test_cases_PGI_serial_${outFormat}
+ cd ${FILE_NAME}_test_cases_PGI_serial_${outFormat}
+ eval set UPPPath = ${RunPath}\'${FILE_NAME}_PGI_serial/\'
+ eval set CasesDir = ${RunPath}\'${FILE_NAME}_test_cases_PGI_serial_${outFormat}/\'
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+ module purge
+ module load pgi/17.1
+ module load netcdf/3.6.3
+ set WRF_DIR='/scratch3/BMC/wrf-chem/KaYee/PRECOMPILED_WRF/WRFV3.9_PGI_serial_large-file/'
+ else if($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
+ module purge
+ module load pgi netcdf ncarcompilers
+ set WRF_DIR='/gpfs/fs1/p/ral/jntp/UPP/PRE_COMPILED_CODE/WRFV3.9_PGI_serial_large-file'
+ module load grads
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+ source /home/hertneky/wheezy-pgi.csh
+ set WRF_DIR='/d1/hertneky/wtf_upp/Builds/WRFV3.7.1.1/em_real/WRFV3'
+ endif
+ endif
+ rm -rf case*
 #
  set bb = 1
  while ($bb <= $numR)
@@ -163,15 +234,31 @@
  sed -i -e "/export/s|incrementhr=[^ ]*|incrementhr=$incrementhr|" run_unipostandgrads
  endif
 
- if($CONFIG_OPTION[$ii] == 4 || $CONFIG_OPTION[$ii] == 8|| $CONFIG_OPTION[$ii] == 2)then
+ if($CONFIG_OPTION[$ii] == 2 ||\
+    $CONFIG_OPTION[$ii] == 4 ||\
+    $CONFIG_OPTION[$ii] == 8 ||\
+    $CONFIG_OPTION[$ii] == 12)then
  echo 'CONFIG_OPTION IS MPI ' $CONFIG_OPTION[$ii]
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA") then
+ set run_line='"mpirun -np 4 '${postexec}'/unipost.exe'
+ sed -i -e "/export/s|RUN_COMMAND=[^ ]*|RUN_COMMAND=$run_line|" run_unipostandgrads
+ else if($COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
  set run_line='"mpiexec_mpt '${postexec}'/unipost.exe'
  sed -i -e "/export/s|RUN_COMMAND=[^ ]*|RUN_COMMAND=$run_line|" run_unipostandgrads
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+ set run_line='"mpirun -machinefile machfile -np 6 '${postexec}'/unipost.exe'
+ endif
  else
  echo 'CONFIG_OPTION IS SERIAL ' $CONFIG_OPTION[$ii]
  endif
 
+ if($COMPUTER_OPTION == "theia" || $COMPUTER_OPTION == "THEIA" ||\
+     $COMPUTER_OPTION == "cheyenne" || $COMPUTER_OPTION == "CHEYENNE") then
  qsub ./run_mpi.pbs
+ echo 'submit jobs'
+ else if($COMPUTER_OPTION == "hydra" || $COMPUTER_OPTION == "HYDRA") then
+ ./run_unipostandgrads
+ endif
 
  sleep $sleepT
 
